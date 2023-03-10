3852c2b7001dc49aa879eee63612db274dc93a05
kkpb8 <ganesh.thapa@microwarecorp.com>
2023-03-10 11:09:22 +0545

    update summary

--- src/main/java/com/devglan/utils/ProfileTypeConstants.java
+++
@@ -1,7 +0,0 @@
-package com.devglan.utils;
-
-public class ProfileTypeConstants {
-    public final static String VO="vo";
-    public final static String SHG="shg";
-    public final static String CLF="clf";
-}
